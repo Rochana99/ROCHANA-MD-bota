00f6a7520373485e897ba793f43419ac18370e05
Rochana99 <116473568+Rochana99@users.noreply.github.com>
2025-08-03 16:02:42 +0530

    𝐑𝐎𝐂𝐇𝐀𝐍𝐀 𝐌𝐃

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Const {
+const {
 default: makeWASocket,
 useMultiFileAuthState,
 DisconnectReason,
@@ -8,6 +8,8 @@ fetchLatestBaileysVersion,
 Browsers
 } = require('@whiskeysockets/baileys')
 
+// ඉතිරි code එක එලෙසම තියෙයි
+// ...
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
 const fs = require('fs')
 const P = require('pino')
